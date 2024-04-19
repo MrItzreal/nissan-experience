19e5264d17c34d47482939a17300b9b9843bd5e0
Israel Santana <is.wsie@gmail.com>
2024-04-19 09:22:56 -0400

    Added minor changes to the navbar. Im having issues understanding responsive design with tailwind.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,36 +3,33 @@ import logo from "../assets/logos/logo.svg";
 
 const Navbar = () => {
   return (
-    <nav className="ml-2 p-1 shadow-md text-stone-200">
-      <div className="container">
-        <div className="flex justify-between ml-20">
-          <div>
-            <h1>
-              <img src={logo} />
-            </h1>
-          </div>
-          <ul className=" flex items-center gap-6 ">
-            <li className="inline-block py-4">
-              <a href="blank_">Vehicles</a>
-            </li>
-            <li>
-              <a href="blank_">Shop</a>
-            </li>
-            <li>
-              <a href="blank_">Electric</a>
-            </li>
-            <li>
-              <a href="blank_">Owners</a>
-            </li>
-            <li>
-              <a href="blank_">Dealers</a>
-            </li>
-          </ul>
+    <nav className="text-stone-200 ">
+      <span>
+        <img src={logo} className="fixed top-4 left-4" />
+      </span>
 
-          <div className="flex items-center border border-gray-100 rounded-full h-12 p-2 mt-6	">
-            <a href="blank_">Build & Price</a>
-            <FaArrowRight />
-          </div>
+      <div className="flex items-center justify-end">
+        <ul className=" flex items-center gap-6 mt-12 mr-14 font-medium">
+          <li>
+            <a href="blank_">Vehicles</a>
+          </li>
+          <li>
+            <a href="blank_">Shop</a>
+          </li>
+          <li>
+            <a href="blank_">Electric</a>
+          </li>
+          <li>
+            <a href="blank_">Owners</a>
+          </li>
+          <li>
+            <a href="blank_">Dealers</a>
+          </li>
+        </ul>
+
+        <div className="flex items-center border border-gray-100 rounded-full w-43 h-11 p-4 mt-12 mr-20">
+          <a href="blank_">Build & Price</a>
+          <FaArrowRight className="ml-3" />
         </div>
       </div>
     </nav>
