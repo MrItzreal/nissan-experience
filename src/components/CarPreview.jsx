439a2106fff73c294569c092ce6e8102d4b6c3a2
Israel Santana <is.wsie@gmail.com>
2024-04-23 16:22:22 -0400

    added the paths to each image.

--- src/components/CarPreview.jsx
+++ src/components/CarPreview.jsx
@@ -6,7 +6,7 @@ import kicks from "../assets/thumbnails/kicks-thumb.avif";
 import murano from "../assets/thumbnails/murano-thumb.avif";
 import armada from "../assets/thumbnails/armada-thumb.avif";
 
-const cars = [
+const vehicles = [
   { name: "Rogue", image: rogue, alt: "Rogue" },
   { name: "Pathfinder", image: pathfinder, alt: "Pathfinder" },
   { name: "Ariya", image: ariya, alt: "Ariya" },
@@ -25,23 +25,26 @@ const categories = [
 
 const CarPreview = () => {
   return (
+    // categories
     <main className="flex justify-center">
       <nav className="absolute">
         <div className="categories flex justify-center gap-6 mt-6 tracking-wide	">
           {categories.map((category) => (
-            <button key={category}>{category.title}</button>
-          ))}
-        </div>
-
-        {/* Thumbnails for vehicles */}
-        <div className="thumbnails flex mt-5">
-          {cars.map((car) => (
-            <button key={car} className="flex relative justify-center">
-              <img src={car.image} alt={car.alt} className="w-44" />
-              <p className="absolute bottom-1 text-xs">{car.name}®</p>
+            <button key={category} className="hover:underline">
+              {category.title}
             </button>
           ))}
         </div>
+
+        {/* Thumbnails for vehicles  */}
+        <section className="thumbnails flex mt-5">
+          {vehicles.map((vehicle) => (
+            <button key={vehicle} className="flex relative justify-center ">
+              <img src={vehicle.image} alt={vehicle.alt} className="w-44" />
+              <p className="absolute bottom-1 text-xs">{vehicle.name}®</p>
+            </button>
+          ))}
+        </section>
         <div className="flex justify-center text-2xl text-gray-500 mt-2 ">
           <IoChevronDownOutline />
         </div>
