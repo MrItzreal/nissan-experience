a729f8e3ec3207db189a695317293f27d4011a5d
Israel Santana <is.wsie@gmail.com>
2024-04-23 14:49:01 -0400

    Used the map function with an array to generate buttons efficiently, avoiding code repetition.

--- src/components/CarPreview.jsx
+++ src/components/CarPreview.jsx
@@ -6,44 +6,41 @@ import kicks from "../assets/thumbnails/kicks-thumb.avif";
 import murano from "../assets/thumbnails/murano-thumb.avif";
 import armada from "../assets/thumbnails/armada-thumb.avif";
 
+const cars = [
+  { name: "Rogue", image: rogue, alt: "Rogue" },
+  { name: "Pathfinder", image: pathfinder, alt: "Pathfinder" },
+  { name: "Ariya", image: ariya, alt: "Ariya" },
+  { name: "Kicks", image: kicks, alt: "Kicks" },
+  { name: "Murano", image: murano, alt: "Murano" },
+  { name: "Armada", image: armada, alt: "Armada" },
+];
+
+const categories = [
+  { title: "Crossover & SUVs" },
+  { title: "Cars" },
+  { title: "Trucks" },
+  { title: "Sports Cars" },
+  { title: "Electric Cars" },
+];
+
 const CarPreview = () => {
   return (
     <main className="flex justify-center">
       <nav className="absolute">
         <div className="categories flex justify-center gap-6 mt-6 tracking-wide	">
-          <button>Crossover & SUVs</button>
-          <button>Cars</button>
-          <button>Trucks</button>
-          <button>Sports Cars</button>
-          <button>Electric Cars</button>
+          {categories.map((category) => (
+            <button key={category}>{category.title}</button>
+          ))}
         </div>
 
         {/* Thumbnails for vehicles */}
         <div className="thumbnails flex mt-5">
-          <button className="flex relative justify-center">
-            <img src={rogue} alt="Rogue" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Rogue®</p>
-          </button>
-          <button className="flex relative justify-center">
-            <img src={pathfinder} alt="Pathfinder" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Pathfinder®</p>
-          </button>
-          <button className="flex relative justify-center">
-            <img src={ariya} alt="Ariya" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Ariya®</p>
-          </button>
-          <button className="flex relative justify-center">
-            <img src={kicks} alt="Kicks" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Kicks®</p>
-          </button>
-          <button className="flex relative justify-center">
-            <img src={murano} alt="Murano" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Murano®</p>
-          </button>
-          <button className="flex relative justify-center">
-            <img src={armada} alt="Armada" className="w-44" />
-            <p className="absolute bottom-1 text-xs">Armada®</p>
-          </button>
+          {cars.map((car) => (
+            <button key={car} className="flex relative justify-center">
+              <img src={car.image} alt={car.alt} className="w-44" />
+              <p className="absolute bottom-1 text-xs">{car.name}®</p>
+            </button>
+          ))}
         </div>
         <div className="flex justify-center text-2xl text-gray-500 mt-2 ">
           <IoChevronDownOutline />
