5569ec683d91446fd0518ae5686a5ace01f95b24
Israel Santana <is.wsie@gmail.com>
2024-05-09 09:24:53 -0400

    Improved the UI of the carGrids and used a map array to display all vehicles.

--- src/components/CarGrids.jsx
+++ src/components/CarGrids.jsx
@@ -1,36 +1,49 @@
+/* eslint-disable react/no-unescaped-entities */
 // import { useState } from "react";
-// import carData from "../data/cars.json";
-
-import rogueprofile from "../assets/profilephotos/rogue-profile.avif";
+import carData from "../data/cars.json";
 
 const CarGrids = () => {
+  //This will display all vehicles through map array.
+  const displayCars = carData.cars;
+
   return (
     <main className="bg-neutral-900">
-      <div className="container mx-auto px-8 py-14 pt-6 ">
-        <h6 className="text-white whitespace-nowrap font-bold text-sm md:text-lg">
-          Crossover & SUVs
+      <div className="container mx-auto px-8 py-2">
+        <h6 className="flex justify-center text-center text-white font-bold text-base md:text-lg">
+          Your lifestyle. Your Nissan!
         </h6>
+        <p className="flex justify-center text-center text-white text-base md:text-lg">
+          Whether you're a new driver, a family adventurer, a creature of
+          comfort or just about any other kind of driver,
+          <br /> there's a Nissan with the tech, the fuel efficiency and the
+          features you need to drive your way.
+        </p>
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6 justify-center gap-6">
-          <div className="relative mb-6">
-            <img src={rogueprofile} className="rounded-2xl border" />
-            <div className="absolute bottom-0 left-0 right-0 my-24 ml-8">
-              <div className="flex flex-col items-center mr-6 -my-4 whitespace-nowrap">
-                <h2 className="text-white text-xs  md:text-base">
-                  2024 NISSAN ROGUE
-                </h2>
-                <h2 className=" text-white text-xs md:text-base ">
-                  Starting at $28,000
-                </h2>
-                <h2 className="text-white text-xs md:text-base ">
-                  Trims: FWD/AWD
-                </h2>
+          {displayCars.map((car) => (
+            <div key={car.id} className="relative mb-12">
+              <img
+                src={car.profilephoto}
+                className="rounded-2xl border h-full"
+              />
+              <div className="absolute bottom-0 left-0 right-0 my-24 ml-8">
+                <div className="flex flex-col items-center mr-6 -my-4 whitespace-nowrap">
+                  <h2 className="text-white text-xs md:text-base">
+                    2024 NISSAN {car.model}
+                  </h2>
+                  <h2 className=" text-white text-xs md:text-base ">
+                    Starting at ${car.price}
+                  </h2>
+                  <h2 className="text-white text-xs md:text-base ">
+                    Trims: FWD/AWD
+                  </h2>
+                </div>
               </div>
-            </div>
 
-            <button className="absolute inset-x-0 bottom-0 h-16 p-2  rounded-b-lg border  border-gray-100 bg-gray-800 hover:bg-red-700 duration-700">
-              <p className="text-white text-base md:text-lg">Buy Now</p>
-            </button>
-          </div>
+              <button className="absolute bottom-0 left-0 h-16  w-full  rounded border border-gray-100 bg-gray-800 hover:bg-red-700 duration-700">
+                <p className="text-white text-base md:text-lg">Details</p>
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </main>
@@ -38,9 +51,3 @@ const CarGrids = () => {
 };
 
 export default CarGrids;
-
-// <img src={pathfinderprofile} className="relative rounded-lg h-5/6" />
-// <img src={ariyaprofile} className="relative rounded-lg h-5/6" />
-// <img src={kickprofile} className="relative rounded-lg h-5/6" />
-// <img src={muranoprofile} className="relative rounded-lg h-5/6" />
-// <img src={armadaprofile} className="relative rounded-lg h-5/6" />
